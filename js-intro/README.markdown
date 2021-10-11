6aca6ca1e6bdd61b26fc3bd9d5c2be0e3217e784
Ravi teja <raviteja0817@gmail.com>
2021-10-12 00:13:53 +0530

    my code

---
+++ js-intro/README.markdown
@@ -0,0 +1,5 @@
+# JS intro
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/raviteja0817/pen/BadyLLr](https://codepen.io/raviteja0817/pen/BadyLLr).
+
+
---
+++ js-intro/dist/script.js
@@ -0,0 +1,93 @@
+console.log("adding of numbers")
+var a=5
+var b=15
+var c=5
+a+b+c
+var sum = a+b+c
+console.log(sum)
+
+
+console.log("substraction of two numbers")
+var diff= b-c
+console.log(diff)
+
+
+console.log("type of variable")
+var d='ravi'
+var e=10
+console.log(typeof(d))
+console.log(typeof(e))
+
+
+console.log("product of numbers")
+var pro=a*c
+console.log(pro)
+
+
+console.log("division of numbers")
+var div=b/c
+console.log(div)
+
+
+console.log("converting number to string")
+console.log(c.toString())
+
+
+console.log("converting string to number")
+var strf="20"
+console.log(parseInt(strf))
+
+
+console.log("converting string to float number")
+var strg="22.7"
+console.log(parseFloat(strg))
+
+
+
+console.log("round it off to given position in float")
+var strfloat=2.674
+console.log(strfloat.toFixed(0))
+console.log(strfloat.toFixed(2))
+
+
+var details="my name is ravi teja"
+console.log(typeof(details))
+console.log()
+
+
+var doublequote="my name is \"ravi teja\""
+console.log(doublequote)
+
+
+console.log("length of string")
+var mystring="my name is ravi teja is"
+console.log(mystring.length)
+
+
+console.log("index of a string")
+console.log(mystring.indexOf('name'))
+console.log(mystring.lastIndexOf('is'))
+
+
+console.log(mystring.slice(11,21))
+console.log(mystring.slice(-15))
+console.log(mystring.slice(3))
+
+
+console.log("string to upper case")
+console.log(mystring.toUpperCase())
+
+
+console.log("string to lower case")
+console.log(mystring.toLowerCase())
+
+
+console.log("merging two strings")
+var one="Ravi"
+var two="Teja"
+console.log(one+' '+two)
+
+
+console.log("split argument")
+var mystring1="my 1 name 1 is 1 ravi teja 1 is"
+console.log(mystring1.split(1))
